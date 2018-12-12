2249913d826be39e2fd0d4bce24589dc41f518d6
brittanyk30 <36874236+BrittanyK30@users.noreply.github.com>
2018-12-12 13:13:10 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//Responsive Navigation
 $(function() {
   menu = $("nav ul");
 
@@ -9,16 +8,37 @@ $(function() {
 
   $(window).resize(function() {
     var w = $(this).width();
-    if (w > 580 && menu.is(":hidden")) {
+    if (w > 480 && menu.is(":hidden")) {
       menu.removeAttr("style");
     }
   });
 
   $("nav li").on("click", function(e) {
     var w = $(window).width();
-    if (w < 580) {
+    if (w < 480) {
       menu.slideToggle();
     }
   });
   $(".open-menu").height($(window).height());
+});
+
+
+//smooth scroll
+
+$("cf a").on("click", function(e){
+ if(this.has != "";) {
+ event.preventDefault();
+
+ const hash = this.hash;
+
+ $("html, body").animate(
+   {
+   scrollTop: $(hash).offset().top,
+  },
+  800,
+  funtion() { 
+    window.location.hash = hash;
+  }
+  );
+}
 });
